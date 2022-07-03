79b9a1ebf450bb18d35c23e2da0b8b3717ea8e59
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-07-03 22:36:19 +0200

    get and redirect and 404 page

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/* app.get('/', (req, res) => {
+  res.send('<h1> Hello World! Ali 2 44455 sll reerrerew <\h1>')
+}) */
+app.get('/html', (req, res) => {
+  res.send('<h1> Learn HTML<\h1>')
+})
 app.get('/', (req, res) => {
-  res.send('Hello World! Ali 2 44455 sll reerrerew')
+  res.redirect('/html')
+})
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
 })
 
 app.listen(port, () => {
