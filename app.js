8b5967e175b89bd56da47cea66290647f0819ee0
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2022-07-07 23:03:32 +0200

    add conect with MogoDb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 
 // auto refresh
 const path = require("path");
@@ -20,12 +21,33 @@ liveReloadServer.server.once("connection", () => {
   }, 100);
 }); 
 
+// connect MongoDB
+ 
+const mongoose = require('mongoose');
+ 
+mongoose.connect("mongodb+srv://ali-ajjoub:ali.node1984@cluster0.pxc5n.mongodb.net/?retryWrites=true&w=majority")
+  .then( result => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    });
+  })
+  .catch( err => {
+    console.log(err);
+  }); 
+
 /* app.get('/', (req, res) => {
   res.send('<h1> Hello World! Ali 2 44455 sll reerrerew <\h1>')
 }) */
 app.get('/', (req, res) => {
   res.render("index")
 })
+app.get('/all-articles', (req, res) => {
+  res.render("index")
+})
+app.get('/add-new-article', (req, res) => {
+  res.render("add-new-article")
+})
+
 /* app.get('/', (req, res) => {
   res.redirect('/html')
 }) */
@@ -34,6 +56,3 @@ app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
